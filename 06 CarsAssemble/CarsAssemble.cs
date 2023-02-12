fbf4f2ae4dda7d756819a4d0226fe3ce31a3873d
CalembourIII <paulius.paulaitis@gmail.com>
2023-02-12 22:51:15 +0200

    Add 1 new done exercise

--- 06 CarsAssemble/CarsAssemble.cs
+++ 06 CarsAssemble/CarsAssemble.cs
@@ -6,10 +6,44 @@ using System.Threading.Tasks;
 
 namespace _06_CarsAssemble
 {
-    internal class Program
+    internal class CarsAssemble
     {
-        static void Main(string[] args)
+        public static double SuccessRate(int speed)
         {
+            if (speed >= 1 && speed <= 4)
+            {
+                return 1.0;
+            }
+
+            if (speed >= 5 && speed <= 8)
+            {
+                return 0.9;
+            }
+
+            if (speed == 9)
+            {
+                return 0.8;
+            }
+
+            if (speed == 10)
+            {
+                return 0.77;
+            }
+
+            else
+            {
+                return 0;
+            }
+        }
+
+        public static double ProductionRatePerHour(int speed)
+        {
+            return (221 * speed) * SuccessRate(speed);
+        }
+
+        public static int WorkingItemsPerMinute(int speed)
+        {
+            return System.Convert.ToInt32((int)ProductionRatePerHour(speed) / 60);
         }
     }
 }

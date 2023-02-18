6b825478cc68f43d686c53f15907bd799693a073
CalembourIII <paulius.paulaitis@gmail.com>
2023-02-19 00:01:22 +0200

    Add excercise "Roll The Die" (done)

---
+++ 11 RollTheDie/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_RollTheDie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public int RollDie()
+        {
+            Random dieRoll = new Random();
+            return dieRoll.Next(1, 19);
+        }
+
+        public double GenerateSpellStrength()
+        {
+            Random spellStrength = new Random();
+            return spellStrength.Next(0, 100);
+        }
+    }
+}

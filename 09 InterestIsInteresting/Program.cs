24abb9a0cdddb759979ebfa2984403362c60ccd6
CalembourIII <paulius.paulaitis@gmail.com>
2023-02-18 23:18:37 +0200

    Add excercise "Interest is Interesting" (done)

---
+++ 09 InterestIsInteresting/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_InterestIsInteresting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static float InterestRate(decimal balance)
+        {
+            if (balance < 0)
+            {
+                return 3.213f;
+            }
+            else if (balance >= 0 && balance < 1000)
+            {
+                return 0.5f;
+            }
+            else if (balance >= 1000 && balance < 5000)
+            {
+                return 1.621f;
+            }
+            else
+            {
+                return 2.475f;
+            }
+        }
+
+        public static decimal Interest(decimal balance)
+        {
+            //decimal interestRate = Convert.ToDecimal(InterestRate(balance));
+            //return balance * (interestRate / 100);
+            return balance * (Convert.ToDecimal(InterestRate(balance)) / 100);
+        }
+
+        public static decimal AnnualBalanceUpdate(decimal balance)
+        {
+            //decimal interest = Interest(balance);
+            //return balance + interest;
+            return Interest(balance) + balance;
+        }
+
+        public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
+        {
+            int yearsToTarget = 0;
+            while (balance < targetBalance)
+            {
+                balance = AnnualBalanceUpdate(balance);
+                ++yearsToTarget;
+            }
+            return yearsToTarget;
+        }
+    }
+}
